8b3c0e42210bcb388f6798ec801b925bb625d871
Alien-oyi <daijinmin78@gmail.com>
2023-03-31 20:24:38 -0400

    Thoughts models and formatdate helper

---
+++ models/Thoughts.js
@@ -0,0 +1,64 @@
+const {Schema,model,Types} = require("mongoose")
+const format_date = require("../utils/helpers")
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type:String,
+            required:true,
+            length:[1,150]
+        },
+        created_at:{
+            type:Date,
+            default:Date.now,
+            get:(timestamp)=>format_date(timestamp)
+        },
+        username:{
+            type:String,
+            required:true
+        },
+        reactions:[ReactionSchema]        
+    },{
+        toJSON:{
+            virtuals:true,
+            getters:true
+        },
+        id:false
+    }
+)
+
+const ReactionSchema = new Schema(
+    {
+        reactionId:{
+            // ObjectId data type
+            type:Schema.Types.ObjectId,
+            default:() =>new Types.ObjectId()
+        },
+        reactionContent:{
+            type:String,
+            required:true,
+            length:[1,150]
+        },
+        username:{
+            type:String,
+            required:true
+        },
+        created_at:{
+            type:Date,
+            default:Date.now,
+            get:(timestamp) =>format_date(timestamp)
+        }
+    },{
+        toJSON:{
+            getters:true,
+        },
+        id:false
+    }
+)
+ThoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length
+})
+
+const Thought = model("Thought",ThoughtSchema);
+
+module.exports = Thought;
